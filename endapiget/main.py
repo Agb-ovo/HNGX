19552204fbb14e9aad1d70d5ae53a04b2bd19095
Agb-ovo <122666353+Agb-ovo@users.noreply.github.com>
2023-09-11 16:05:46 +0100

    Update main.py

--- endapiget/main.py
+++ endapiget/main.py
@@ -4,20 +4,20 @@ import pytz
 
 app = FastAPI()
 
-@app.get("/info") 
+@app.get("/") 
 async def get_info(
-    name: str = Query(None),
-    course: str = Query(None)  
+    slack_name: str = "agbrokoogheneovo",
+    course: str = "backend" 
 ):
 
     day = datetime.now(pytz.utc).strftime("%A")
     time = datetime.utcnow().isoformat() + 'Z'
 
-    file_url = "https://github.com/username/repo/blob/main/main.py"
-    repo_url = "https://github.com/username/repo"
+    file_url = "https://github.com/Agb-ovo/HNGX/blob/b8ee70f1375a5d79b95cdfc064fa0687d3b81e1e/endapiget/main.py"
+    repo_url = "https://github.com/Agb-ovo/HNGX.git"
 
     return {
-        "name": name,
+        "name": slack_name,
         "day": day,
         "time": time,
         "course": course,
